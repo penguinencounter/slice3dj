3441f10e3133ac86c3195bba65e6f2cba800c000
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-07-12 11:30:07 -0700

    bonus printing utility

---
+++ printall.lua
@@ -0,0 +1,14 @@
+local args = {...}
+if #args ~= 1 then
+    printError("usage: printall <dir>")
+    return
+end
+
+local dir = args[1]
+if not dir:find("^/") then dir = shell.dir() .. "/" .. dir end
+if not dir:find("/$") then dir = dir .. "/" end
+
+local files = fs.list(dir)
+for _, file in ipairs(files) do
+    shell.run("print3d " .. dir .. file)
+end
